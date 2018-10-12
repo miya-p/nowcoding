b2a570e61adc6a66d002329052e3da2cbd0e4653
[mijin] <[sonetive@gmail.com]>
2018-10-12 10:32:33 +0900

    수정

--- assets/js/project.js
+++ assets/js/project.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 						$("body").removeClass("fix");
 						viewPrjContent.css('display', 'none');
 					});
-                    
+
 					$(".project_view.mob .left .block").css("height", "600px");
 
                 } else {
@@ -53,5 +53,22 @@ $(document).ready(function(){
         });
         $(window).resize();
     });
+
+
+	/* 새 프로젝트 만들기 FAB 스크롤 제어 */
+	window.onscroll = function() {
+		var d = document.documentElement;
+		var footHeight = $("footer").outerHeight();
+		var offset = d.scrollTop + window.innerHeight + footHeight;
+		var height = d.offsetHeight;
+		var stopHeight = d.offsetHeight - footHeight - 115;
+
+		if (offset >= height) {
+			$("#fab-wp").css({"position":"absolute", "top": stopHeight + "px"});
+		}else{
+			$("#fab-wp").removeAttr("style");
+			$("#fab-wp").css({"position":"fixed", "bottom":"35px"});
+		}
+	};
 
 });
